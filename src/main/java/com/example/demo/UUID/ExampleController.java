5cf4940ff7b89f6865cb48a3e9556545be0bb045
EvhenyiErokhin <erohin0611@gmail.com>
2022-09-15 07:18:31 +0300

    rename class

--- src/main/java/com/example/demo/UUID/ExampleController.java
+++ src/main/java/com/example/demo/UUID/ExampleController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SomeController {
+public class ExampleController {
     @GetMapping("/")
     public String Text(Model model){
         model.addAttribute("text", "very importent text");
